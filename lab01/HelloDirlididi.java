4f8d0121fdf0146a8307e5f461fac42cad9393a3
Luciano Nascimento <luciano.nascimento.filho@gmail.com>
2023-08-03 00:11:33 -0300

    remocao dos arquivos de p1 e adicao do diretorio de p2 para labs

---
+++ lab01/HelloDirlididi.java
@@ -0,0 +1,11 @@
+/**
+* Laboratório de Programação 2 - Lab 1
+*
+* @author Luciano Souza do Nascimento Filho - 122210214
+*/
+
+public class HelloDirlididi {
+    public static void main(String args[]) {
+        System.out.println("Hello Dirlididi!");
+    }
+}
